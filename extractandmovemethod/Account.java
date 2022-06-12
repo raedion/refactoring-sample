d71fd621e423f89d46fb83c4320b2f7588eaabbf
raedion <u548026g@ecs.osakau.ac.jp>
2022-06-12 18:20:24 +0900

    move method

--- extractandmovemethod/Account.java
+++ extractandmovemethod/Account.java
@@ -2,12 +2,8 @@ public class Account {
     public int a = 3;
     public int b = 8;
     public int getAccount() {
-        output();
+        Output output = new Output();
+        output.output(this.a, this.b);
         return this.a + this.b;
-    }
-    private void output() {
-        if (this.a < this.b) {
-            System.out.println("a < b");
-        }
     }
 }
---
+++ extractandmovemethod/Output.java
@@ -0,0 +1,7 @@
+public class Output {
+    public void output(int a, int b) {
+        if (a < b) {
+            System.out.println("a < b");
+        }
+    }
+}

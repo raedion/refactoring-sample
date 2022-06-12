d88dafd687fd8ce002228b546d363ae0ca42c445
raedion <u548026g@ecs.osakau.ac.jp>
2022-06-12 20:15:49 +0900

    extract and move method in a file

--- extractandmovemethod/Account.java
+++ extractandmovemethod/Account.java
@@ -2,9 +2,15 @@ public class Account {
     public int a = 3;
     public int b = 8;
     public int getAccount() {
-        if (this.a < this.b) {
-            System.out.println("a < b");
-        }
+        Output output = new Output();
+        output.output(this.a, this.b);
         return this.a + this.b;
+    }
+    class Output {
+        public void output(int a, int b) {
+            if (a < b) {
+                System.out.println("a < b");
+            }
+        }
     }
 }

0e05c8a7f1bf73392616edce31e8d06db7f8e147
raedion <u548026g@ecs.osakau.ac.jp>
2022-06-12 18:16:37 +0900

    extract method

--- extractandmovemethod/Account.java
+++ extractandmovemethod/Account.java
@@ -2,9 +2,12 @@ public class Account {
     public int a = 3;
     public int b = 8;
     public int getAccount() {
+        output();
+        return this.a + this.b;
+    }
+    private void output() {
         if (this.a < this.b) {
             System.out.println("a < b");
         }
-        return this.a + this.b;
     }
 }
